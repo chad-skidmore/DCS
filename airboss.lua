6662cc5e9b5b43a4968f9856110986630ba7f310
Chad Skidmore <chad@skidmore.org>
2026-06-10 15:04:51 -0700

    Update airboss.lua

--- airboss.lua
+++ airboss.lua
@@ -88,7 +88,7 @@ end
 function StartRescueHelo()
     if Rescuehelo:IsStopped() then
         Rescuehelo:Start()
-        TIMER:New(RescueheloState):Start(5, 30)
+        TIMER:New(RescueheloState):Start(60, 30)
     end
 end
 
